23c98bd9fd50e712749d6aa0018ff590847a7cf2
borgr <ktilana@gmail.com>
2017-08-01 09:13:52 +0300

    r script

--- hierarchy.R
+++ hierarchy.R
@@ -2,10 +2,18 @@ require(cluster)
 
 
 temp <- list.files("clustering/distance_metrics", pattern="*.csv", full.names = TRUE)
-my.files <- lapply(temp, read.csv, header = T)
+my.files <- lapply(temp, read.csv, header = T, row.names = 1)
 my.dists <-lapply(my.files, as.dist)
 res <-lapply(my.files, diana)
 res <-lapply(my.files, plot)
 
-
-my.data <- read.csv('clustering/distance_metrics/aligned.csv', header = T, row.names = T)
+dir <- 'clustering/distance_metrics/'
+filename <- 'aligned.csv'
+filename <-'bag_cosine.csv'
+filename <-'bigram_cosine.csv'
+filename <-'inv_hamming.csv'
+filename <-''
+filename <-''
+my.data <- read.csv(paste(dir, filename, sep=""), header = T, row.names = 1)
+my.dist <- as.dist(my.data)
+plot(diana(my.dist))
